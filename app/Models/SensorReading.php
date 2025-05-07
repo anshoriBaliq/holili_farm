f767de6a39c0ed0a93e02a0a89efe4d595714998
iqbalansh <alanshoriiqbal@gmail.com>
2025-05-07 17:35:04 +0700

    push api to realtime

--- app/Models/SensorReading.php
+++ app/Models/SensorReading.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SensorReading extends Model
+class SensorReading extends BaseModel
 {
     protected $table = 'sensor_readings';
-    protected $fillable = ['temperature', 'ppm', 'water_level', 'created_at', 'updated_at'];
+    protected $fillable = ['temperature', 'ppm', 'water_level'];
 }
