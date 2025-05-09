38b830080ebfe83964da870eb20caedf22506308
iqbalansh <alanshoriiqbal@gmail.com>
2025-05-09 14:18:54 +0700

    Revert "Revert "Revert "push api to realtime"""
    This reverts commit 66e2e3ddc06aa0589acaaa1d6ab912e31902b0dd.

--- app/Models/SensorReading.php
+++ app/Models/SensorReading.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SensorReading extends BaseModel
+class SensorReading extends Model
 {
     protected $table = 'sensor_readings';
-    protected $fillable = ['temperature', 'ppm', 'water_level'];
+    protected $fillable = ['temperature', 'ppm', 'water_level', 'created_at', 'updated_at'];
 }
